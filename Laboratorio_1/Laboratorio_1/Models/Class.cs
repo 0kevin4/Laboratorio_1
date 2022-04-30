e4a05396b70f9b50d1a7e2aa7a76c91cc08c3239
Kevin Moreno <kevin.moreno249@misena.edu.co>
2022-04-30 15:10:57 -0500

    copy

---
+++ Laboratorio_1/Laboratorio_1/Models/Class.cs
@@ -0,0 +1,11 @@
+﻿namespace Laboratorio_1.Models
+{
+    public class Class
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Designation { get; set; }
+        public string Adress    { get; set; }
+        public DateTime? RecordCreation { get; set; }
+    }
+}
