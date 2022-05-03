fb98fbb659c5330a49067748091862779376607a
Kevin Moreno <kevin.moreno249@misena.edu.co>
2022-05-03 11:56:50 -0500

    Reinicio

--- Laboratorio_1/Laboratorio_1/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Laboratorio_1.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
